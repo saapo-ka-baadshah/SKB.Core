4d99181e52ba37fb1a7ec178269ee389fba264b4
Amey Mahadik <ameyarm@gmail.com>
2025-05-26 21:19:01 +0200

    Added Instrumentation Support to the Hosting Package
    - Added Service Declaration for Traces
    - Added Service Declaration for Metrics

---
+++ src/Hosting/Extensions/Instrumentations/InstrumentationExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SKB.Core.Hosting.Extensions.Instrumentations;
+
+/// <summary>
+/// Adds the injectable Core Instrumentations to the service collections
+/// </summary>
+public static class InstrumentationExtensions
+{
+
+	/// <summary>
+	/// Adds the instrumentation sources to the service collection to inject as Dependencies
+	/// </summary>
+	/// <param name="services">Service Collection <see cref="IServiceCollection"/></param>
+	/// <returns>Extended Service Collection</returns>
+	public static IServiceCollection AddInstrumentation<TAssembly>(this IServiceCollection services)
+	{
+		// Add Activity Source for Tracing
+		services.AddScoped<ActivitySource>(sp => new ActivitySource(
+			$"{typeof(TAssembly).Namespace}.Activity"
+			));
+
+		// Add Metering Source for Metrics
+		services.AddScoped<Meter>(sp => new Meter(
+			$"{typeof(TAssembly).Namespace}.Meter"
+			));
+		return services;
+	}
+}

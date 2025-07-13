f7b73c9aa20eccf88c6737a45b72f70482d7d455
Amey Mahadik <36587413+saapo-ka-baadshah@users.noreply.github.com>
2025-07-13 17:45:56 +0200

    Updated the config to a fixed null, added non async policies

--- src/Policies/Reactive/RetryFamily/RetryPolicy.cs
+++ src/Policies/Reactive/RetryFamily/RetryPolicy.cs
@@ -11,22 +11,73 @@ namespace SKB.Core.Policies.Reactive.RetryFamily;
 [PublicAPI]
 public static class RetryPolicy
 {
+	/// <summary>
+	/// Adds a count limited retry policy
+	///     NOTE: Defaulted to Exponential backoff
+	/// </summary>
+	/// <param name="config">Provided application settings configuration</param>
+	/// <typeparam name="TError">Type of error to be handled</typeparam>
+	/// <returns>Limited Count retry policy</returns>
+	[PublicAPI]
+	public static Policy CountLimitedRetryPolicy<TError>(
+			IConfiguration? config = null
+		)
+	where TError : Exception
+	{
+		// load the correct retry policy options
+		RetryPolicyOptions options = GetRetryPolicyOptions(config);
+
+		if (options.Jitter)
+		{
+			return Policy
+				.Handle<TError>()
+				.WaitAndRetry(
+					Backoff.DecorrelatedJitterBackoffV2(
+						options.DecoratedJitterMedian,
+						options.MaxRetries
+					)
+				);
+		}
+
+		if (options.BackoffExponential)
+		{
+			// Defualt Retry to prevent the physical network flooding with exponential backoff policy
+			return Policy
+				.Handle<TError>()
+				.WaitAndRetry(
+					Backoff.ExponentialBackoff(
+						options.InitialDelay,
+						options.MaxRetries
+					)
+				);
+		}
+
+		// default case should always be the linear backoff, but will be overriden by the configuration
+		return Policy
+			.Handle<TError>()
+			.WaitAndRetry(
+				Backoff.LinearBackoff(
+					options.InitialDelay,
+					options.MaxRetries
+				)
+			);
+	}
+
     /// <summary>
     /// Adds a count limited retry policy
     ///     NOTE: Defaulted to Exponential backoff
     /// </summary>
+    /// <param name="config">Provided application settings configuration</param>
+    /// <typeparam name="TError">Type of error to be handled</typeparam>
     /// <returns>Limited Count retry policy</returns>
     [PublicAPI]
     public static IAsyncPolicy CountLimitedRetryPolicyAsync<TError>(
-            IConfiguration? config
+            IConfiguration? config = null
         )
     where TError : Exception
     {
         // load the correct retry policy options
-        RetryPolicyOptions options = config!
-            .GetSection(RetryPolicyOptions.RetryPolicyOptionsKey)
-            .Get<RetryPolicyOptions>() 
-            ?? new RetryPolicyOptions();
+        RetryPolicyOptions options = GetRetryPolicyOptions(config);
 
         if (options.Jitter)
         {
@@ -52,7 +103,7 @@ public static class RetryPolicy
                     )
                 );
         }
-        
+
         // default case should always be the linear backoff, but will be overriden by the configuration
         return Policy
             .Handle<TError>()
@@ -71,22 +122,52 @@ public static class RetryPolicy
     /// <param name="config">Provided application settings configuration</param>
     /// <typeparam name="TError">Type of error to be handled</typeparam>
     /// <returns>forever retry policy</returns>
+    [PublicAPI]
+    public static Policy ForeverRetryPolicy<TError>(
+	    IConfiguration? config = null
+    )
+	    where TError: Exception
+    {
+	    // load the correct retry policy options
+	    RetryPolicyOptions options = GetRetryPolicyOptions(config);
+
+	    return Policy
+		    .Handle<TError>()
+		    .WaitAndRetryForever(_ =>
+			    options.ForeverSleepDuration
+		    );
+    }
+
+    /// <summary>
+    /// Adds a forever retry policy
+    ///     NOTE: Defaulted to Exponential backoff
+    /// </summary>
+    /// <param name="config">Provided application settings configuration</param>
+    /// <typeparam name="TError">Type of error to be handled</typeparam>
+    /// <returns>forever retry policy</returns>
+    [PublicAPI]
     public static IAsyncPolicy ForeverRetryPolicyAsync<TError>(
-        IConfiguration? config
+        IConfiguration? config = null
         )
     where TError: Exception
     {
         // load the correct retry policy options
-        RetryPolicyOptions options = config!
-                                         .GetSection(RetryPolicyOptions.RetryPolicyOptionsKey)
-                                         .Get<RetryPolicyOptions>() 
-                                     ?? new RetryPolicyOptions();
-        
+        RetryPolicyOptions options = GetRetryPolicyOptions(config);
+
         return Policy
             .Handle<TError>()
-            .WaitAndRetryForeverAsync(_ => 
+            .WaitAndRetryForeverAsync(_ =>
                     options.ForeverSleepDuration
                 );
     }
-    
+
+    private static RetryPolicyOptions GetRetryPolicyOptions(IConfiguration? config = null)
+    {
+	    return config is null
+		    ? new RetryPolicyOptions()
+		    : config!
+			      .GetSection(RetryPolicyOptions.RetryPolicyOptionsKey)
+			      .Get<RetryPolicyOptions>()
+		      ?? new RetryPolicyOptions();
+    }
 }

56113c5ad152921af8fbe75e1681dfb287be18d2
Amey Mahadik <36587413+saapo-ka-baadshah@users.noreply.github.com>
2025-07-19 20:45:03 +0200

    Add web application builder support for OpenTelemetry
    - Added an overload for the AddOpenTelemetry method

--- src/Hosting/Extensions/OpenTelemetry/OpenTelemetryExtensions.cs
+++ src/Hosting/Extensions/OpenTelemetry/OpenTelemetryExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using OpenTelemetry.Exporter;
 using OpenTelemetry.Logs;
@@ -20,6 +22,85 @@ public static class OpenTelemetryExtensions
 	///  </param>
 	public static void AddOpenTelemetry(
 		this IHostApplicationBuilder builder
+		)
+	{
+		string otlpEndpoint = builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"] ?? "";
+		bool useOtlpEndpoint = !string.IsNullOrEmpty(otlpEndpoint);
+		Action<OtlpExporterOptions>? otlpExporterOptionsAction = null;
+		if (useOtlpEndpoint)
+		{
+			if (Uri.TryCreate(otlpEndpoint, UriKind.Absolute, out var otlpUri))
+			{
+				otlpExporterOptionsAction = (options) =>
+				{
+					options.Endpoint = otlpUri;
+					// defaults to gRPC
+					// defaults to No Auth
+				};
+			}
+			else
+			{
+				Console.WriteLine($"[WARNING] Invalid OtlpEndpoint specified: {otlpEndpoint}");
+				useOtlpEndpoint = false;
+			}
+		}
+		if (!useOtlpEndpoint)
+		{
+			Console.WriteLine($"[WARNING] Skipping OtlpExport because OTLP endpoint is not configured. Falling back to default ConsoleExporter.");
+		}
+
+		// Configure Trace
+		builder.Services.ConfigureOpenTelemetryTracerProvider(providerBuilder =>
+		{
+			if (useOtlpEndpoint && otlpExporterOptionsAction != null)
+			{
+				providerBuilder.AddOtlpExporter(otlpExporterOptionsAction);
+			}
+			else
+			{
+				providerBuilder.AddConsoleExporter();
+			}
+		});
+
+		// Configure Metrics
+		builder.Services.ConfigureOpenTelemetryMeterProvider(providerBuilder =>
+		{
+			if (useOtlpEndpoint && otlpExporterOptionsAction != null)
+			{
+				providerBuilder.AddOtlpExporter(otlpExporterOptionsAction);
+			}
+			else
+			{
+				providerBuilder.AddConsoleExporter();
+			}
+		});
+
+		// Configure Logging
+		builder.Logging.AddOpenTelemetry(loggingOptions =>
+		{
+			loggingOptions.IncludeFormattedMessage = true;
+			loggingOptions.IncludeScopes = true;
+			loggingOptions.ParseStateValues = true;
+
+			if (useOtlpEndpoint && otlpExporterOptionsAction != null)
+			{
+				loggingOptions.AddOtlpExporter(otlpExporterOptionsAction);
+			}
+			else
+			{
+				loggingOptions.AddConsoleExporter();
+			}
+		});
+	}
+
+	///  <summary>
+	///  Add and Configure Open Telemetry Extensions
+	///  </summary>
+	///  <param name="builder">
+	/// 		Provided Application Builder
+	///  </param>
+	public static void AddOpenTelemetry(
+		this WebApplicationBuilder builder
 		)
 	{
 		string otlpEndpoint = builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"] ?? "";
